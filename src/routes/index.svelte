50931d0dd16006d0b47e3f1a3a112f07c9fb46b9
Steinberg <steinberg@hey.com>
2021-06-08 22:17:28 +0200

    Add README as index content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,28 @@
+<script context="module">
+	export async function preload () {
+		const res = await this.fetch(`/README.md.json`);
+
+		if (res.status === 200) {
+			const page = await res.json();
+			return { page };
+		}
+
+		this.error(404, 'Not found');
+	}
+</script>
+
+<script>
+	export let page;
+</script>
+
 <svelte:head>
-	<title>Index</title>
+	{#if page && !page.length && page.title}
+		<title>{page.title}</title>
+	{/if}
 </svelte:head>
 
-<h1>Index</h1>
+{#if page && !page.length}
+	<h1>{@html page.title}</h1>
+	<p>{@html page.deck}</p>
+	{@html page.body}
+{/if}
