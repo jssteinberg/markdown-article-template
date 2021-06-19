9169209dcf22a8580bdaae3860ec3b2b567c5300
Steinberg <steinberg@hey.com>
2021-06-19 22:39:24 +0200

    Fixed implementation title sort

--- src/routes/[...type].json.js
+++ src/routes/[...type].json.js
@@ -23,7 +23,7 @@ export async function get(req, res) {
 			type: 'date',
 			index: -1, // index to use for sorting in properties that are arrays
 		},
-		inFolder: true,
+		inFolder: !isFile,
 		// longOutput: true,
 		// sortBy: [
 		// 	{
